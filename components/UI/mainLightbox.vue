7c6c911b305b022db17de794910d65654cafa631
abdken1359 <abdielkengne6@gmail.com>
2025-04-28 00:36:24 -0300

    almost done with showcase lightbox

--- components/UI/mainLightbox.vue
+++ components/UI/mainLightbox.vue
@@ -18,21 +18,26 @@
             <Icon name="fe:close" class="text-3xl md:text-2xl lg:text-4xl"/>
         </button>
         <section class="flex align-center gap-1 md:gap-4 a-container h-[80vh] md:h-[80vh] mx-auto">
-            <button class="relative  h-max my-auto w-max mr-auto disabled:text-gray-400 flex-1">
+            <button class="relative  h-max my-auto w-max mr-auto disabled:text-gray-400 disabled:cursor-auto flex-1
+            " @click.stop="utils.previousImage()"
+            :disabled="utils.isPreviousButtonDisabled"
+            >
                 <Icon name="fe:arrow-left" class="text-xl md:text-4xl lg:text-5xl"/>
             </button>
             <Transition name="lightbox">
-            <img :src="utils.imageInView" :alt="utils.imageInViewAlt" v-show="utils.isLightboxVisible" 
-            class="mx-auto md:flex-1 h-[30vh]  md:h-auto my-auto md:w-1/2"/>
+            <img :src="utils.imageQueue[utils.imageLightBoxIndex]?.image" :alt="utils.imageInViewAlt" v-show="utils.isLightboxVisible" 
+            class="mx-auto md:flex-1 h-[30vh]   md:h-auto my-auto md:w-1/2"/>
         </Transition>
-            <button class="relative h-max my-auto ml-auto disabled:text-gray-400 flex-1">
+            <button class="relative h-max my-auto ml-auto disabled:text-gray-400 disabled:cursor-auto flex-1" 
+            @click.stop="utils.nextImage()" :disabled="utils.isNextButtonDisabled">
                 <Icon name="fe:arrow-right" class="text-xl md:text-4xl lg:text-5xl"/>
             </button>
             
         </section>
         <section class="flex a-container items-center flex-wrap justify-center gap-4">
             <template v-for="i in utils.imageQueue" :key="i.id">
-                <img :src="i.image" :alt="i.alt" class="w-20 h-20"/>
+                <img :src="i.image" :alt="i.alt" class=" md:w-15 cursor-pointer hover:opacity-90 md:h-15 lg:w-20 lg:h-20" 
+                :class="i.inview?'':' opacity-25'"  @click.stop="utils.goToImage(i.image,utils.imageQueue)"/>
             </template>
         </section>
     </div>
@@ -55,5 +60,21 @@
     }
 })*/
 const utils=useUtilitiesStore();
+/*onMounted(()=>{
+    window.addEventListener('keydown',(e)=>{
+        if (e.key==="ArrowRight"){
+            
+            if(utils.imageLightBoxIndex===utils.imageQueue.length-1){
+                e.preventDefault()
+
+            }else{
+                utils.nextImage()
+            }
+        }
+        if(e.key==="ArrowLeft"){
+            utils.previousImage()
+        }
+    })
+})*/
 
 </script>
--- stores/utilitiesStore.ts
+++ stores/utilitiesStore.ts
@@ -21,18 +21,81 @@ export const useUtilitiesStore=defineStore('UtilitiesStore',()=>{
     const imageInView=ref<String>('/Images/space.jpg')
     const imageInViewAlt=ref<String>('heythere')
     const imageQueue=ref<Object[]>([])
+    const imageLightBoxIndex=ref<Number>(0)
+    const isNextButtonDisabled=computed(()=>{
+        if(imageLightBoxIndex.value===imageQueue.value.length-1){
+            return true
+        }else{
+            return false
+        }
+    })
+    const isPreviousButtonDisabled=computed(()=>{
+        if(imageLightBoxIndex.value===0){
+            return true
+        }else{
+            return false
+        }
+    })
     const showLightBox=(UserImageInView:string,UserImageInViewAlt:string,UserImageQueue:Object[]):void=>{
         isLightboxVisible.value=true
         imageInView.value=UserImageInView
         imageInViewAlt.value=UserImageInViewAlt
         imageQueue.value=UserImageQueue
+        imageLightBoxIndex.value=UserImageQueue.findIndex(u => u.image === UserImageInView);
+        UserImageQueue.forEach(u => {
+             u.image===UserImageInView?u.inview=true:u.inview=false
+             
+
+        });
+        console.log(imageLightBoxIndex.value)
 
     }
+    const nextImage=():void=>{
+          imageLightBoxIndex.value+=1
+          imageInView.value = imageQueue.value[imageLightBoxIndex.value].image;
+          imageInViewAlt.value = imageQueue.value[imageLightBoxIndex.value].alt;
+          imageQueue.value.forEach(i => {
+            i.image===imageInView.value?i.inview=true:i.inview=false
+
+                
+
+            
+          });
+         
+    }
+    const previousImage=():void=>{
+        imageLightBoxIndex.value-=1
+        imageInView.value = imageQueue.value[imageLightBoxIndex.value].image;
+          imageInViewAlt.value = imageQueue.value[imageLightBoxIndex.value].alt;
+          imageQueue.value.forEach(i => {
+            i.image===imageInView.value?i.inview=true:i.inview=false
+
+                
+
+            
+          });
+        
+  }
+  const goToImage=(userImageView:string,userImagesecondQueue:object[])=>{
+
+    imageLightBoxIndex.value=userImagesecondQueue.findIndex(u => u.image === userImageView);
+    imageInView.value = imageQueue.value[imageLightBoxIndex.value].image;
+    imageInViewAlt.value = imageQueue.value[imageLightBoxIndex.value].alt;
+    imageQueue.value.forEach(i => {
+        i.image===imageInView.value?i.inview=true:i.inview=false
+
+            
+
+        
+      });
+
+
+  }
     const closeLightBox=():void=>{
         isLightboxVisible.value=false
         imageInView.value=''
         imageInViewAlt.value=''
-        imageQueue.value.length=0
+        
     }
     return{
         isImageModalVisible,
@@ -46,5 +109,11 @@ export const useUtilitiesStore=defineStore('UtilitiesStore',()=>{
         imageInView,
         imageInViewAlt,
         imageQueue,
+        imageLightBoxIndex,
+        nextImage,
+        previousImage,
+        isNextButtonDisabled,
+        isPreviousButtonDisabled,
+        goToImage
     }
 })
