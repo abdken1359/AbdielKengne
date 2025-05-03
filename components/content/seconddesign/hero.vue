343fdee5c23e34ea7a3ac19132c04fad3dd6f61c
abdken1359 <abdielkengne6@gmail.com>
2025-05-02 22:32:28 -0300

    Starting hero

---
+++ components/content/seconddesign/hero.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <div class="a-container flex my-5 md:my-10 items-center lg:w-[90%] flex-col lg:flex-row gap-4">
+        <div class="w-full lg:w-1/2">
+            <p>Hi, I am</p>
+            <h1 class=" text-6xl font-[Poppins]">Abdiel Kengne</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus aliquid minima unde dolorem officia, totam assumenda nisi reiciendis officiis sequi deleniti expedita, veritatis fugit aliquam aut dolores nihil ducimus veniam.</p>
+        </div>
+        <div class="w-full lg:w-1/2">
+            <NuxtImg src="/Images/heropurple.png"/>
+        </div>
+        </div>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
+    <div>
+        <ContentSeconddesignHero/>
     <div class="a-container">
         <h1>Hello world!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deserunt eaque quibusdam ea corporis repellendus obcaecati blanditiis quidem molestiae. Repellendus illo cupiditate praesentium? Fugit quo necessitatibus, laudantium esse vero expedita!</p>
+    </div>
     </div>
 </template>
 <script lang="ts" setup>
