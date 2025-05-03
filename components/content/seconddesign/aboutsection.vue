5494e609fdb062a1251d6af87fe0c53258641559
abdken1359 <abdielkengne6@gmail.com>
2025-05-03 19:42:48 -0300

    Starting to implement Complex Naigation

--- components/content/seconddesign/aboutsection.vue
+++ components/content/seconddesign/aboutsection.vue
@@ -1,16 +1,20 @@
 <template>
-    <section class="bg-gray-50 border-y-2 border-gray-200">
-        <div class="a-container py-10 grid grid-cols-1 gap-4 items-center">
+    <section class="bg-gray-50 border-y-1 border-gray-200">
+        
+        <div class="a-container py-5 ">
+            <h1 class="a-container">About</h1>
+            <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center">
+            <div>
+            <NuxtImg src="/Images/youngman.png" class=" outline-2 outline-sky-600"/>
+        </div>
         <div>
-            <h1>A Glimpse into my personality</h1>
-            <p>I'm a self-taught frontend web developer and graphic designer with a passion for crafting thoughtful digital experiences. Currently in junior high school, I’ve been independently learning modern web technologies like Nuxt 3 and Tailwind CSS, along with visual design tools like Canva. I enjoy blending creativity with code to build clean, responsive, and user-focused interfaces. My goal is to grow as a full-stack developer while continuing to explore UI/UX design and brand identity. I'm always curious, always learning, and excited about what comes next.
-
-</p> 
+           <h2>Bio</h2>
+            <p>I'm a self-taught frontend web developer and graphic designer with a passion for crafting thoughtful digital experiences.</p> 
         </div>
-        <div class="hidden">
-            <NuxtImg src="/Images/youngman.png"/>
-        </div>
+    </section>
+        
            
         </div>
+        
     </section>
 </template>
