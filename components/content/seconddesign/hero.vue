6095df9e400219e33321da6759712e5e2bc0411c
abdken1359 <abdielkengne6@gmail.com>
2025-05-03 01:09:22 -0300

    Debugging hero section

--- components/content/seconddesign/hero.vue
+++ components/content/seconddesign/hero.vue
@@ -1,10 +1,11 @@
 <template>
     <section>
-        <div class="a-container flex my-5 md:my-10 items-center lg:w-[90%] flex-col lg:flex-row gap-4">
-        <div class="w-full lg:w-1/2">
+        <div class="flex my-5 md:my-10 items-center lg:w-[90%] flex-col lg:flex-row gap-4">
+        <div class="w-full a-container  lg:w-1/2">
             <p>Hi, I am</p>
             <h1 class=" text-5xl md:text-6xl lg:text-7xl font-[Poppins]">Abdiel Kengne</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus aliquid minima unde dolorem officia, totam assumenda nisi reiciendis officiis sequi deleniti expedita, veritatis fugit aliquam aut dolores nihil ducimus veniam.</p>
+            <p>I am a curious, enthusiastic, and visionary frontend web developer, graphic designer, and UI/UX specialist with a creative mindset for
+                 crafting impactful experiences.</p>
             <div class="my-4 grid items-center grid-cols-1 md:grid-cols-2  gap-4 ">
                 <template v-for="l in littleabout" :key="l.id">
                     <div>
@@ -15,7 +16,7 @@
             </div>
             <button class=" bg-blue-600 hover:bg-blue-500 hidden lg:flex text-white">Download CV</button>
         </div>
-        <div class="w-full lg:w-1/2">
+        <div class="w-full lg:a-container lg:w-1/2">
             <NuxtImg src="/Images/heroblue.png" alt="Aesthetic image of graphic design elements"/>
         </div>
         </div>
@@ -27,7 +28,7 @@ const littleabout=ref([
         id:1,title:'UI/UX Specialist', subtitle:'A seasoned UI/UX Strategist dedicated to bridging creativity'
     },
     {
-        id:2,title:'Graphic Designer',subtitle:'Designing appealing and impactful graphics.'
+        id:2,title:'Creative Visualist',subtitle:'Designing appealing and impactful graphics.'
     },
    /* {
         id:3,title:'Canada',subtitle:'Based'
