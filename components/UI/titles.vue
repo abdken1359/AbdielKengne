b39e3dcd8308cb6bd035b5c9c58f3e1f781cde92
abdken1359 <abdielkengne6@gmail.com>
2025-05-04 13:22:44 -0300

    Starting nav menu

--- components/UI/titles.vue
+++ components/UI/titles.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="group w-max mt-5 sm:mb-14 lg:mb-12 lg:my-10">
+    <div class="group w-max mt-5 mb-10 lg:mb-12 lg:my-10">
 
         <h2 
-        class="relative after:absolute after:w-1/2 after:h-3 after:bg-blue-500 after:-bottom-4 after:left-0"
+        class="relative after:absolute after:w-1/2 after:h-2 after:bg-blue-500 after:-bottom-2 after:left-0"
         ><span class="mr-2 group-hover:text-blue-500 hidden transition-all duration-100
               ">#</span><NuxtLink :href="link" class="group-hover:translate-y-10">{{ text }}</NuxtLink></h2>
     </div>
