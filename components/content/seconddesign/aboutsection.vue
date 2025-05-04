e53eb9cdca9b4afc726970e906705855a1bf8786
abdken1359 <abdielkengne6@gmail.com>
2025-05-04 00:10:04 -0300

    making about section

--- components/content/seconddesign/aboutsection.vue
+++ components/content/seconddesign/aboutsection.vue
@@ -3,18 +3,28 @@
         
         <div class="a-container py-5 ">
             <h1 class="a-container" >About</h1>
-            <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center">
+            <section class="grid grid-cols-1 lg:grid-cols-2  gap-4 items-center">
             <div>
-            <NuxtImg src="/Images/youngman.png" class=" outline-2 outline-sky-600" alt="Young man"/>
+            <NuxtImg src="/Images/youngman.png" class=" md:w-[70%] lg:w-[90%] outline-2 outline-sky-600" alt="Young man"/>
         </div>
         <div>
-           <h2>Bio</h2>
-            <p>I’m a self-taught developer with a strong curiosity for how things work and
+           <h2>Who am I</h2>
+            <p ref="aboutsection" id="about">I’m a self-taught developer with a strong curiosity for how things work and
                  a deep interest in building for the web. What began as a hobby quickly grew into a focused journey of learning and creating. I’ve spent countless hours exploring online tutorials, experimenting with small projects, and developing my problem-solving mindset. I enjoy the process of turning ideas into real, usable experiences and continuously push myself to learn,
                  improve, and build with purpose.</p> 
         </div>
-        <div>
-            <h2 ref="aboutsection" id="about">Skills</h2>
+        <div class="hidden">
+            <h2 >Skills</h2>
+            <div class="grid grid-cols-1 grid-rows-1 md:grid-rows-2 md:grid-cols-2 gap-4 items-center">
+                <template v-for="s in skills" :key="s.id">
+                <div class="flex gap-4 items-center justify-center">
+                    <p class="w-1/4">{{ s.name }}</p>
+                    <div class="w-3/4 p-2 bg-gray-300"></div>
+
+                </div>
+               </template>
+            </div>
+           
         </div>
     </section>
         
@@ -40,7 +50,22 @@ watch(isAboutVisible,(newVal)=>{
 })
 const skills=ref<Skills[]>([
     {
-
+         id:1,
+         name:'HTML',
+         color:'orange-500',
+         length:95,
+    },
+    {
+         id:2,
+         name:'CSS',
+         color:'blue-500',
+         length:85,
+    },
+    {
+         id:3,
+         name:'Javascript',
+         color:'yellow-500',
+         length:90,
     }
 ])
 </script>
