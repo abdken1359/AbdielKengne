c8faf3fa84c9b49a6670449d31889b373b56ecaf
abdken1359 <abdielkengne6@gmail.com>
2025-05-05 01:50:19 -0300

    Implemenmted skills section intersect

--- components/content/seconddesign/skills.vue
+++ components/content/seconddesign/skills.vue
@@ -1,8 +1,18 @@
 <template>
      <section class="a-container py-2" id="skills">
         <UITitles text="Skills" link="#skills"/>
-        <p>Below is a list of skills which define my passion.</p>
+        <p ref="skillsSection">Below is a list of skills which define my passion.</p>
         <UICompetenciesTab/>
 
     </section>
 </template>
+<script setup lang="ts">
+const useful=useUsefulData()
+const skillsSection=ref(null)
+const isSkillsSectionVisible=useElementVisibility(skillsSection)
+watch(isSkillsSectionVisible,(newVal)=>{
+    if (newVal==true){
+        useful.dealWithIntersects(3)
+    }
+})
+</script>
