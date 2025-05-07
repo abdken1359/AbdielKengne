538578ddc71ab04ae0ec1748f1ebd5037d99c608
abdken1359 <abdielkengne6@gmail.com>
2025-05-07 10:19:02 -0300

    Starting showcase section

--- components/content/seconddesign/services.vue
+++ components/content/seconddesign/services.vue
@@ -16,7 +16,7 @@
                         </div>
                     </div>
                 </template>
-            </section>
+            </section> 
             </div>
     </section>
 </template>
